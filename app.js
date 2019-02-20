ab544297a844e4dc9665268728e14ac6f595acfa
Saathvik Thandra <saathvikt@orbdomain>
2019-02-20 13:56:58 +0530

    webpack config

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/DataAPI', { useNewUrlParser: true });
 const port = process.env.PORT || 3000;
 
-const SetOfPosts = require('./Models/postsmodel');
-const setOfComments = require('./Models/commentsmodel');
+const SetOfPosts = require('./models/postsmodel');
+const setOfComments = require('./models/commentsmodel');
 
-const commentsRouter = require('./Routes/commentsRouter')(setOfComments);
-const postRouter = require('./Routes/postRouter')(SetOfPosts);
+const commentsRouter = require('./routes/commentsRouter')(setOfComments);
+const postRouter = require('./routes/postRouter')(SetOfPosts);
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
